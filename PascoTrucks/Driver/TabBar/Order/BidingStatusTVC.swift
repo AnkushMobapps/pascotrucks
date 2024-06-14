1494c4a7ff6c79bfe576c2439a2c96717568a1c6
AnkushMobapps <mobappssolutions138@gmail.com>
2024-06-14 18:23:34 +0530

    14/06/24

---
+++ PascoTrucks/Driver/TabBar/Order/BidingStatusTVC.swift
@@ -0,0 +1,23 @@
+//
+//  BidingStatusTVC.swift
+//  PascoTrucks
+//
+//  Created by Narendra Kumar on 13/06/24.
+//
+
+import UIKit
+
+class BidingStatusTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

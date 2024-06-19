af8b4d31b6b65318ebc4fddf0f60c9160a859525
AnkushMobapps <mobappssolutions138@gmail.com>
2024-06-19 17:30:08 +0530

    19/06/2024  today code shireen

---
+++ PascoTrucks/Driver/TabBar/DriverHome/HorigontalStackOptions/Emergency/DriverEmergencyCell.swift
@@ -0,0 +1,23 @@
+//
+//  DriverEmergencyCell.swift
+//  PascoTrucks
+//
+//  Created by Narendra Kumar on 18/06/24.
+//
+
+import UIKit
+
+class DriverEmergencyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
